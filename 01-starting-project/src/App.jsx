806fa491525f8f023c2034eed5e83eb088ec5c5d
shen <zero258014@gmail.com>
2025-12-12 17:48:32 +0900

    use state to update UI.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
+  const [tab, setTab] = useState("default content.");
+  function handleChangeTab(selectedTab) {
+    setTab(selectedTab);
+    console.log(tab);
+  }
+
   return (
     <div>
       <Header />
@@ -26,11 +33,18 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton changeTab={() => handleChangeTab("Components")}>
+              Components
+            </TabButton>
+            <TabButton changeTab={() => handleChangeTab("JSX")}>JSX</TabButton>
+            <TabButton changeTab={() => handleChangeTab("Props")}>
+              Props
+            </TabButton>
+            <TabButton changeTab={() => handleChangeTab("State")}>
+              State
+            </TabButton>
           </menu>
+          {tab}
         </section>
       </main>
     </div>
